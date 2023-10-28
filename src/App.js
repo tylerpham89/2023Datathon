835fabac81f3760354905940551251dc1ce24bc7
Daniel <danielmpang@gmail.com>
2023-10-28 10:56:02 -0500

    push

--- src/App.js
+++ src/App.js
@@ -22,4 +22,20 @@ function App() {
   );
 }
 
-export default App;
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+// export default MyButton;
